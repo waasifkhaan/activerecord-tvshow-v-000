19fdbf178f8815f50a2f51308046d3f187974171
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 03:06:05 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -20,7 +20,7 @@ class Show < ActiveRecord::Base
     Show.where("rating > 5")
   end 
   def self.shows_by_alphabetical_order
-    Show.order(name: :desc).first
+    Show.order(name: :asc)
   end 
   
 end
